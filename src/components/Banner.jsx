1ced42599223ab98c785aab6ff7efeed58c10b0e
Lemmy Mwaura <lemminton@gmail.com>
2022-07-05 15:51:47 +0300

    feat: implements fetch functionality of the  movie banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,25 @@
-import React from "react"
+import { useRef } from "react"
+import { useQuery } from "react-query"
+import axios from "../utils/Axios"
+import requests from "../utils/Requests"
 
 export default function Banner() {
+  const movie = useRef(null)
+  const { fetchNetflixOriginals } = requests
+
+  const { data: request, isLoading } = useQuery("banner-movie", () =>
+    axios.get(fetchNetflixOriginals)
+  )
+
+  if (!isLoading) {
+    movie.current =
+      request?.data?.results[
+        Math.floor(Math.random() * request?.data?.results.length - 1)
+      ]
+  }
+
+  console.log(movie.current)
+
   const truncate = (str, maxLength) => {
     if (str?.length > maxLength) {
       return str.substring(0, maxLength) + "..."
@@ -14,13 +33,20 @@ export default function Banner() {
       style={{
         backgroundSize: "cover",
         backgroundPosition: "center center",
-        backgroundImage:
-          "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWUKZrbq4b_VanaYIN0xGPi7ZGWn93nrs7vQ&usqp=CAU)",
+        backgroundImage: `url(https://image.tmdb.org/t/p/original/${
+          movie.current?.backdrop_path || ""
+        })`,
       }}
     >
       <div className="banner-details">
-        <h1 className="banner-title">Title</h1>
-        <p className="banner-description">{truncate("Description", 100)}</p>
+        <h1 className="banner-title">
+          {movie.current?.name ||
+            movie.current?.title ||
+            movie.current?.original_name}
+        </h1>
+        <p className="banner-description">
+          {truncate(`${movie.current?.overview}`, 180)}
+        </p>
         <div className="banner-buttons">
           <button className="btn btn-primary">Play</button>
           <button className="btn btn-sec">More Info</button>
