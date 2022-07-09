ac8f3ebef70252363df7b9b9716f38b9be318357
Lemmy Mwaura <lemminton@gmail.com>
2022-07-09 13:40:42 +0300

    feat: modal and swiper setup

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,11 +1,30 @@
 import { useQuery } from "react-query"
+import { useSelector, useDispatch } from "react-redux"
 import axios from "../utils/Axios"
 
+//swiper
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Navigation } from "swiper"
+import "swiper/css"
+import "swiper/css/navigation"
+
+//Modal
+import { toggleModal, setMovie } from "../features/modalSlice"
+import Modal from "./Modal"
+
 export default function Row({ title, fetchUrl, isLargeRow = false }) {
+  const dispatch = useDispatch()
+  const showModal = useSelector((state) => state.modal.isOpen)
   const baseURL = "https://image.tmdb.org/t/p/original/"
+
   const { data: request, isLoading } = useQuery(title, () =>
     axios.get(fetchUrl)
   )
+
+  const movieData = (movie) => {
+    dispatch(setMovie(movie))
+    dispatch(toggleModal())
+  }
 
   if (isLoading) {
     return <div>Loading...</div>
@@ -13,12 +32,26 @@ export default function Row({ title, fetchUrl, isLargeRow = false }) {
 
   return (
     <div className="movie-category">
+      {showModal && <Modal/>}
       <h1 className="category-title">{title}</h1>
-      <div className="movies-container">
+      <Swiper
+        className="movies-container mySwiper"
+        slidesPerView={isLargeRow ? 5 : 5}
+        spaceBetween={0}
+        slidesPerGroup={2}
+        loop={true}
+        navigation={true}
+        modules={[Navigation]}
+      >
         {request?.data?.results?.map((movie, index) => {
           return (
-            movie.poster_path && movie.backdrop_path && (
-              <div key={index} className="movie">
+            movie.poster_path &&
+            movie.backdrop_path && (
+              <SwiperSlide
+                key={index}
+                className="movie"
+                onClick={() => movieData(movie)}
+              >
                 <img
                   className={`movie-image ${
                     isLargeRow && " movie-image-large"
@@ -28,11 +61,11 @@ export default function Row({ title, fetchUrl, isLargeRow = false }) {
                   }`}
                   alt={`${movie.title || movie.name}'s image`}
                 />
-              </div>
+              </SwiperSlide>
             )
           )
         })}
-      </div>
+      </Swiper>
     </div>
   )
 }
